2f647b4db0a8ed4a256d4ef35482e2c4e3cc9674
WhiteEuphoria <prognikitos@gmail.com>
2022-09-02 00:58:49 +0300

    improvement of the mobile version

--- js/main.js
+++ js/main.js
@@ -41,19 +41,38 @@ const burgerBtn = document.querySelector(".burger");
 
 modalBtn.addEventListener("click", function (){
     document.querySelector(".modal").classList.toggle("modal-active");
+      document.querySelector(".header__logo").classList.toggle("header__logo--mobile");
+       document.querySelector(".header-logo").classList.toggle("logo-mobile");
+         document.querySelector(".header__nav").classList.toggle("attr-mobile");
 });
 modalsBtn.addEventListener("click", function (){
     document.querySelector(".modal").classList.toggle("modal-active");
+    document.querySelector(".header__logo").classList.toggle("header__logo--mobile");
+    document.querySelector(".header-logo").classList.toggle("logo-mobile");
+    document.querySelector(".header__nav").classList.toggle("attr-mobile");
 });
 offerModalBtn.addEventListener("click", function (){
     document.querySelector(".modal").classList.toggle("modal-active");
+    document.querySelector(".header__logo").classList.toggle("header__logo--mobile");
+    document.querySelector(".header-logo").classList.toggle("logo-mobile");
+    document.querySelector(".header__nav").classList.toggle("attr-mobile");
 });
 closeBtn.addEventListener("click", function (){
     document.querySelector(".modal").classList.remove("modal-active");
+    document.querySelector(".header__logo").classList.toggle("header__logo--mobile");
+     document.querySelector(".header-logo").classList.toggle("logo-mobile");
+    document.querySelector(".header__nav").classList.toggle("attr-mobile");
+
+
 });
 burgerBtn.addEventListener("click", function (){
     document.querySelector(".burger").classList.toggle("burger--active");
+    document.querySelector(".nav-menu").classList.toggle("nav-menu--active");
+    document.querySelector(".header__nav--menu").classList.toggle("header__nav--active");
+    document.querySelector(".call-btn").classList.toggle("call-btn--active");
 });
+
+
 let screenWindows = screen.width;
 let block = document.querySelector(".footer__wrapp");
 let elem1 = document.querySelector(".footer__links")
@@ -63,4 +82,10 @@ let elem2 = document.querySelector(".footer__time")
     block.insertBefore(wrap, elem2)    
     wrap.append(elem1)
     wrap.appendChild(elem2)
+  }
+let screenW = screen.width;
+let block1 = document.querySelector(".header__nav--menu");
+let elem = document.querySelector(".call-btn")
+  if (screenW <= 400) {
+    block1.append(elem) 
   }
